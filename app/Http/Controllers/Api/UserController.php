6a623db65a59799158727bd588f8d0a716b93836
loglones <goose9098@gmail.com>
2026-01-31 10:29:43 +0300

    сделал круд апи для юзера ,реквесты, ресурсы,контроллер и пути

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserStoreRequest;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Resources\UserResource;
+use App\Http\Requests\UserUpdateRequest;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserStoreRequest $request)
+    {
+        $created_user = User::create($request->validated());
+        return new UserResource($created_user);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UserUpdateRequest $request, User $user)
+    {
+        $data =$request->validated();
+        if($request->has('password')) {
+            $data['password'] = Hash::make($request->password);
+        }
+        $user->update($data);
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response(null,204);
+    }
+}
