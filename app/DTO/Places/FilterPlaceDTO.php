e5217475dfa853f9a97e79d09e0a66a58a3b29a9
loglones <goose9098@gmail.com>
2026-03-13 14:20:52 +0300

    изменил фото в миграции на нуллейбел что бы можно было удалять,добавил фильтрацию по дате,вывод помещений со слотами по времени,добавил сваггер на свое апи

--- app/DTO/Places/FilterPlaceDTO.php
+++ app/DTO/Places/FilterPlaceDTO.php
@@ -2,12 +2,14 @@
 namespace App\DTO\Places;
 
 use App\DTO\BaseDTO;
+use Carbon\CarbonImmutable;
 
 class FilterPlaceDTO extends BaseDTO
 {
     public function __construct(
         public ?float $min_price = null,
         public ?float $max_price = null,
+        public ?CarbonImmutable $date = null,
     )
     {
 
