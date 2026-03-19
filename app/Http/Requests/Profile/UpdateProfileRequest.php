dfe8eb683265d31afa19c9afc6cd9f64e331b97c
merkis88 <axuenno88@gmail.com>
2026-03-19 21:56:23 +0700

    FIX(data): middle name update

--- app/Http/Requests/Profile/UpdateProfileRequest.php
+++ app/Http/Requests/Profile/UpdateProfileRequest.php
@@ -17,7 +17,7 @@ final class UpdateProfileRequest extends FormRequest
         return [
             'first_name' => ['sometimes', 'string', 'max:255'],
             'last_name' => ['sometimes', 'string', 'max:255'],
-            'middle_name' => ['nullable', 'string', 'max:255'],
+            'patronymic' => ['nullable', 'string', 'max:255'],
             'phone' => ['sometimes', 'string', 'max:30', Rule::unique('users', 'phone')->ignore($this->user()?->id)],
             'email' => ['sometimes', 'string', 'email', 'max:255', Rule::unique('users', 'email')->ignore($this->user()?->id)],
         ];
