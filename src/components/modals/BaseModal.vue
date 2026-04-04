4d527c0ba654ca684ba5c0e47cb142885762655d
kostya aga <kostyafan2006@gmail.com>
2026-04-04 13:28:18 +0700

    add 404

--- src/components/modals/BaseModal.vue
+++ src/components/modals/BaseModal.vue
@@ -53,6 +53,7 @@ onUnmounted(() => {
 </script>
 
 <template>
+<<<<<<< Updated upstream
     <Teleport to="body">
         <Transition name="modal">
             <div v-if="modelValue" class="modal-overlay" @click.self="handleBackdropClick">
@@ -69,6 +70,24 @@ onUnmounted(() => {
                             </button>
                             <h3 v-if="title" class="modal-title">{{ title }}</h3>
                         </div>
+=======
+  <Teleport to="body">
+    <Transition name="fade">
+      <div v-if="modelValue" class="modal-overlay" @click.self="handleBackdropClick">
+        <Transition name="modal-scale">
+          <div class="modal-container" :style="{ maxWidth: maxWidth }">
+            <div class="modal-header">
+              <button
+                v-if="showCloseButton"
+                class="modal-back"
+                @click="closeModal()"
+                aria-label="Назад"
+              >
+                <img src="@/assets/images/icons/arrow.svg" alt="" />
+              </button>
+              <h3 v-if="title" class="modal-title">{{ title }}</h3>
+            </div>
+>>>>>>> Stashed changes
 
                         <div class="modal-body">
                             <slot />
